ece30782a02a2faddb078d37ba0309dd760fbe61
Manesha P <119470598+ManeshaMadhu@users.noreply.github.com>
2023-04-20 11:50:44 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,2 +1,29 @@
+const prompt = require('prompt-sync)();
 class subract(){
-  constructre(a,b)
+  constructor(a,b){
+    this.a = a;
+    this.b = b;
+    this.c = a-b;
+  }
+  get_c(){
+    return(this.c);
+  }
+}
+
+function input(){
+  a = parseInt(prompt("Enter 1st number");
+  b = parseInt(prompt("Enter 2nd number");
+  return[a,b];
+}
+
+function output(s){
+  console.log("The difference between %d and %d is %d",s.a,s.b,s.c);
+}
+
+function main(){
+  let a = 0;
+  let b = 0;
+  [a,b] = input();
+  let s = new subract(a,b);
+  output(s);
+}
